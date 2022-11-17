ecd2dd6d2d84122b1f0cda0097aca11e13767d6f
Quviry <kogekao@ya.ru>
2022-11-17 12:13:33 +0300

    EOD

--- expectedafterexpression/views.py
+++ expectedafterexpression/views.py
@@ -4,9 +4,11 @@ from django.core.paginator import Paginator
 
 # Pagination logic
 
-def paginate(object_list= [], page_num=1, per_page=10):
-    paginator = Paginator(object_list, per_page)
-    page_obj = paginator.get_page(page_num)
+def paginate(object_list, request, per_page=10):
+    page = request.GET.get("page", 1)
+    paginator = Paginator(object_list or [], per_page)
+    page_obj = paginator.get_page(page)
+    page_obj.adjusted_elided_pages = paginator.get_elided_page_range(page, on_each_side=2)
     return {'page_obj': page_obj}
 
 # Pagination logic end
@@ -18,29 +20,33 @@ def get_questions():
     return [{
         "title": "title " + str(i),
         "id": i,
-        "text": "text" + str(i)
-    } for i in range(1, 30)]
+        "text": "text" + str(i),
+        "tags": ["first", "second", "third"]
+    } for i in range(1, 1000)]
 
 
 # views
 
 def index(request, page=1):
     request.authorised = True
-    data = paginate(get_questions(), page, 10)
+    data = paginate(get_questions(), request, 10)
     return render(request, "index.html", context=data)
 
 
-def hot(request):
-    request.authorised = False
-    return render(request, "index.html")
+def hot(request, page=1):
+    request.authorised = True
+    data = paginate(get_questions(), request, 10)
+    return render(request, "hot.html", context=data)
 
 
-def tag(request):
-    request.authorised = False
-    return render(request, "index.html")
+def tag(request, tag_name="tag", page=1):
+    request.authorised = True
+    data = paginate(get_questions()[::2], request, 10)
+    data["tag"] = tag_name
+    return render(request, "tag.html", context=data)
 
 
-def question(request, id):
+def question(request, id=1):
     request.authorised = True
     return render(request, "question.html", context={"id": id})
 
